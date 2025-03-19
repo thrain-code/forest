b2ed996190416f2a58a5d6d79fa9d70454e77c14
thrain-traitor <arkadanisaysstuff@gmail.com>
2025-03-20 00:01:48 +0700

    Add commit script

--- commit.sh
+++ commit.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
+# Tambahkan teks otomatis ke log.txt
 echo "Auto commit at $(date)" >> log.txt
+
+# Commit dan push
 git add log.txt
 git commit -m "Automated commit on $(date)"
 git push
